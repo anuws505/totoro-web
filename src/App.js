c2877bbaf4b8efe2b5a9a25903e9141dfe7be28a
anuwas49 <anuwas49@aislocal.dev>
2023-05-09 18:01:12 +0700

    add spinner

--- src/App.js
+++ src/App.js
@@ -1,14 +1,92 @@
-// import logo from './logo.svg';
 import './App.scss';
+import { useState } from 'react';
+// import SpinLoader from './components/SpinLoader/SpinLoader';
 
 function App() {
+  // const flag = true;
+  // const spinn = (flag) ? <SpinLoader /> : '';
+  const totoroJson = {
+    customer: 'guest',
+    sale: 'pom',
+    createAt: '2023-05-09 12:02:31',
+    modifyAt: [
+      '2023-05-09 12:02:31','2023-05-09 12:03:10',
+      '2023-05-09 12:05:11','2023-05-09 13:11:06'
+    ],
+    numbers: [
+      {
+        number: '09',
+        direct: {
+          price: 100,
+          multiple: 70,
+          total: 100,
+          reward: {
+            reward: false,
+            total: 7000
+          }
+        },
+        mix: {
+          price: 100,
+          multiple: 35,
+          total: 100,
+          reward: {
+            reward: false,
+            total: 3500
+          }
+        }
+      },
+      {
+        number: '31',
+        direct: {
+          price: 200,
+          multiple: 70,
+          total: 200,
+          reward: {
+            reward: false,
+            total: 14000
+          }
+        },
+        mix: {
+          price: 200,
+          multiple: 35,
+          total: 200,
+          reward: {
+            reward: false,
+            total: 7000
+          }
+        }
+      }
+    ],
+    grandTotal: 600,
+    tax: {
+      percent: 15,
+      total: 90,
+      grandTotalBeforTax: 510
+    }
+  };
+  const [uname, setUname] = useState('');
+  const [passwd, setPasswd] = useState('');
+
   return (
     <>
       <div className="App">
-        <h1>ภาษาไทย โลเล็ม บลาๆ</h1>
-        <h2>ภาษาไทย โลเล็ม บลาๆ</h2>
-        <h3>ภาษาไทย โลเล็ม บลาๆ</h3>
-        <p>ภาษาไทย โลเล็ม บลาๆ</p>
+        <button type='button'
+        >เพิ่ม</button>
+
+        <div>{JSON.stringify(totoroJson)}</div>
+
+        <input type='text'
+               name='uname'
+               placeholder='uname'
+               value={uname}
+               onChange={(v) => setUname(v.target.value)}
+        />
+        <input type='password'
+               name='passwd'
+               placeholder='passwd'
+               value={passwd}
+               onChange={(v) => setPasswd(v.target.value)}
+        />
       </div>
     </>
   );
---
+++ src/components/SpinLoader/SpinLoader.js
@@ -0,0 +1,11 @@
+import './SpinLoader.scss';
+
+function SpinLoader() {
+  return (
+    <>
+      <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+    </>
+  );
+}
+
+export default SpinLoader;
